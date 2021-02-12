8719fdabb14da43a91dedcf3786851928c883de7
YuetLong Leung <yleung@protonmail.com>
2021-02-12 11:49:19 -0500

    Refactor to use Apollo's loadMore. Refactor client side app to use Provider and useQuery hook.

--- src/App.js
+++ src/App.js
@@ -1,62 +1,52 @@
 import React, { useState, useEffect } from 'react';
 import PersonCard from './PersonCard';
-import { ApolloClient, gql, InMemoryCache} from "@apollo/client";
+import { ApolloClient, gql, InMemoryCache, useQuery } from "@apollo/client";
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 
-const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  uri: "http://localhost:4000/graphql"
-});
+
 
 function App() {
-  const [people, setPeople] = useState([]);
-
-  const personCards = people.map((person, idx) =>
-      <Grid item xs={12} md={4} key={idx}>
-        <PersonCard data={person} />
-      </Grid>
-  );
-
-  const fetchPeople = (first, offset) => {
-    client.query({
-      query: gql`
-        query TestQuery {
-          People (offset: ${offset}, first: ${first}){
-            name
-            homeworld {
-              name
+    const MAIN_QUERY = gql`
+        query TestQuery ($offset: Int!, $first: Int!){
+            People (offset: $offset, first: $first){
+                name
+                homeworld {
+                    name
+                }
+                height
+                mass
+                birth_year
             }
-            height
-            mass
-            birth_year
-          }
         }
-      `
-    })
-    .then(result => {
-      if (result.data.People != null && result.data.People.length > 0){
-        setPeople(people.concat(result.data.People))
-      }
+    `;
+    const { loading, error, data, fetchMore} = useQuery(MAIN_QUERY, {
+        variables: {
+            offset: 0,
+            first: 10
+        }
     });
-  }
-
-
-  const loadMore = () => {
-    fetchPeople(10, people.length);
-  }
-
-  useEffect(() => {loadMore();}, []);
 
   return (
     <div className="App">
-      <Grid container spacing={3}>
-          {personCards}
-          <Button variant="outlined" color="primary" onClick={() => loadMore()}>
-            Load more...
-          </Button>
-      </Grid>
-
+        {loading ? "loading..." :
+            <Grid container spacing={3}>
+                {
+                    data.People.map((person, idx) =>
+                        <Grid item xs={12} md={4} key={idx}>
+                            <PersonCard data={person} />
+                        </Grid>)
+                }
+                <Button variant="outlined" color="primary" onClick={() => fetchMore({
+                    variables: {
+                        offset: data.People.length,
+                        limit: 10
+                    }
+                })}>
+                    Load more...
+                </Button>
+            </Grid>
+        }
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client';
+
+const client = new ApolloClient({
+    cache: new InMemoryCache({
+            typePolicies: {
+                Query: {
+                    fields: {
+                        People: {
+                            keyArgs: [],
+                            merge(existing, incoming, { args: { offset = 0 }}) {
+                                // Slicing is necessary because the existing data is
+                                // immutable, and frozen in development.
+                                const merged = existing ? existing.slice(0) : [];
+                                for (let i = 0; i < incoming.length; ++i) {
+                                    merged[offset + i] = incoming[i];
+                                }
+                                return merged;
+                            },
+                        },
+                    },
+                },
+            },
+        }
+    ),
+    uri: "http://localhost:4000/graphql"
+});
 
 ReactDOM.render(
   <React.StrictMode>
-      <App />
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
